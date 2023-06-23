bb6542719d73011618f657ddb00cbd95d0067c86
Patrick Shobe <patrick@shobe.dev>
2023-06-23 16:00:01 -0600

    initial commit

---
+++ spec/rbar_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rbar do
+  it "has a version number" do
+    expect(Rbar::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
