ed7416349111d2bbbe92d25b24a83ac52f7015d3
Patrick Shobe <patrick@shobe.dev>
2023-09-14 20:40:08 -0600

    add individual classes to handle each type of bar

---
+++ lib/rbar/bars/failed.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# # frozen_string_literal: true
+
+require 'pastel'
+require_relative './base_bar'
+
+module Rbar
+  module Bars
+    class Failed < BaseBar
+      def color
+        Pastel.new.red(marker)
+      end
+
+      def format
+        'Failed  [:bar :percent] :current'
+      end
+    end
+  end
+end
